624c25b958e3fb8f08b9d271f7e4a325d29b355f
violettalivada <vilkalivada@gmail.com>
2020-08-01 03:13:54 +0300

    Модель зарегистрирована в админке.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import GuestBook
 
-# Register your models here.
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'author', 'email', 'created_at']
+    list_filter = ['author', 'created_at']
+    search_fields = ['author']
+    fields = ['author', 'email', 'text', 'created_at', 'updated_at']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
+
