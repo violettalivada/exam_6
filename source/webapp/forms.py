990d4cf2a257b29aeb6acebebd8834103d5c150c
violettalivada <vilkalivada@gmail.com>
2020-08-01 04:29:58 +0300

    Добавлена форма

---
+++ source/webapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class GuestBookForm(forms.Form):
+    author = forms.CharField(max_length=40, required=True, label='Автор', initial='Unknown')
+    email = forms.EmailField(max_length=50, required=True, null=False, blank=False, verbose_name='Email автора')
+    text = forms.CharField(max_length=3000, required=True, label="Текст", widget=forms.Textarea)
+
