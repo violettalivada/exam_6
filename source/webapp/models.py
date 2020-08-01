b8d92f11c70b799c5101ffdae2fed393272822d3
violettalivada <vilkalivada@gmail.com>
2020-08-01 03:04:15 +0300

    Добавлена модель гостевой книги.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = [
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано')
+]
+
+
+class Guestbook(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    email = models.EmailField(max_length=50, null=False, blank=False, verbose_name='Email автора')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='active', verbose_name='Активно')
+
+    def __str__(self):
+        return f'{self.author} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Гостевая книга'
+        verbose_name_plural = 'Гостевые книги'
