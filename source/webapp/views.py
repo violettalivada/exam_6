ac7c32fc992117df6a6e77ed371a60c529647e94
violettalivada <vilkalivada@gmail.com>
2020-08-01 04:00:24 +0300

    добавлено представление главной страницы.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from webapp.models import GuestBook
 
-# Create your views here.
+
+def index_view(request):
+    is_admin = request.GET.get('is_admin', None)
+    if is_admin:
+        data = GuestBook.objects.all()
+    else:
+        data = GuestBook.objects.filter(status='active')
+    return render(request, 'index.html', context={
+        'guestbook': data
+    })
