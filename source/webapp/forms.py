bec888420b3896377969249c8ed7e3c74923115c
violettalivada <vilkalivada@gmail.com>
2020-08-01 05:00:30 +0300

    Исправлены ошибки.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class GuestBookForm(forms.Form):
     author = forms.CharField(max_length=40, required=True, label='Автор', initial='Unknown')
-    email = forms.EmailField(max_length=50, required=True, null=False, blank=False, verbose_name='Email автора')
+    email = forms.EmailField(max_length=50, required=True, label='Email автора')
     text = forms.CharField(max_length=3000, required=True, label="Текст", widget=forms.Textarea)
 
